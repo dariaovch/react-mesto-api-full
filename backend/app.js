7764eb5527679e5251b083bed64c0227ef0439d2
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-12-19 15:27:05 +0100

    cors

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS',
+  );
+  next();
+});
 
 app.use(bodyParser.json());
 
