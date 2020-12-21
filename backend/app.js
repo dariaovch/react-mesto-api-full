f9598dd196287e2226d51d191435cc61e64a4bce
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-12-21 02:58:03 +0100

    fix

--- backend/app.js
+++ backend/app.js
@@ -72,14 +72,19 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.post('/signup', createUser);
-
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
 }), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), createUser);
 
 app.use(auth);
 
