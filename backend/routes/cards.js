d5a9007eee34eaa4ceb0521791ae7b4e7a237eb2
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-12-21 03:08:40 +0100

    id validation

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -19,10 +19,22 @@ router.post('/', celebrate({
   }),
 }), createCard);
 
-router.delete('/:cardId', deleteCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    postId: Joi.string().alphanum().length(24).hex(),
+  }),
+}), deleteCard);
 
-router.put('/:cardId/likes', likeCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    postId: Joi.string().alphanum().length(24).hex(),
+  }),
+}), likeCard);
 
-router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    postId: Joi.string().alphanum().length(24).hex(),
+  }),
+}), dislikeCard);
 
 module.exports = router;
