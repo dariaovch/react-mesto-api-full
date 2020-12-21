6b9d8c4e1473b49c32318618bd58910290e41769
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-12-21 02:52:25 +0100

    fix

--- backend/app.js
+++ backend/app.js
@@ -81,9 +81,6 @@ app.post('/signin', celebrate({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
     email: Joi.string().required().email().unique(),
     password: Joi.string().required().min(6),
   }),
